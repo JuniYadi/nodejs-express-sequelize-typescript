22f5f9592393d8c028ca1f6b80fc0e676a47aeba
JuniYadi <juniyadi94@gmail.com>
2021-06-07 22:31:18 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,38 @@
 import * as express from "express";
-import { NextFunction, Request, Response } from "express";
 import * as cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import { connection } from "./config";
+import router from "./routes";
 
 // init connection to database
 connection()
   .then()
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    /* eslint-disable */
+    console.error(err);
+  });
 
 // init express app
 const app = express.default();
 
 /* Middleware */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors.default());
+app.use(helmet());
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    return res.json({ message: "success" });
-  } catch (e) {
-    next(e);
-  }
-});
+/* Only Use Morgan in Development */
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("combined"));
+}
 
-app.listen(3000, () => {
+/* Default Route */
+app.use(router);
+
+/* Default Port ExpressJS */
+const PORT = process.env.APP_PORT || 3000;
+app.listen(PORT, () => {
   /* eslint-disable */
-  console.log("Server Running in Port 3000");
+  console.log(`Server Running in Port: ${PORT}`);
 });
