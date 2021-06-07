bb19a4e0c90916d50797f9f35ee7d5349574a830
JuniYadi <juniyadi94@gmail.com>
2021-06-07 19:24:38 +0700

    Update Sequelize

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["@typescript-eslint"],
   extends: [
+    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
   parserOptions: {
-    project: path.resolve(__dirname, './tsconfig.json'),
+    project: path.resolve(__dirname, "./tsconfig.json"),
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
   },
   rules: {
-    "no-console": "error",
-    "semi": ["error", "always"],
-    // "quotes": ["error", "double"]
+    "no-console": "off",
+    semi: "error",
+    quotes: ["off", "double"],
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,28 @@
-import * as express from 'express';
-import {NextFunction, Request, Response} from 'express';
-import * as cors from 'cors';
+import * as express from "express";
+import { NextFunction, Request, Response } from "express";
+import * as cors from "cors";
+import { connection } from "./config";
 
+// init connection to database
+connection()
+  .then()
+  .catch((err) => console.error(err));
+
+// init express app
 const app = express.default();
 
 /* Middleware */
 app.use(cors.default());
 
-app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        return res.json({ message: 'success'});
-    } catch (e) {
-        next(e);
-    }
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    return res.json({ message: "success" });
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.listen(3000, () => {
-    /* eslint-disable */
-    console.log('Server Running in Port 3000');
-})
+  /* eslint-disable */
+  console.log("Server Running in Port 3000");
+});
